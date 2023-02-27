cc1b54c3b0b5bc6420693c1c04f8dafc48400663
nassersaazi <nassersnoop@gmail.com>
2023-02-27 13:43:26 +0300

    First notes on chapter 2 - Rustbook

---
+++ rustbook/guessing_game/target/debug/deps/guessing_game-8100b1a168adc73f.d
@@ -0,0 +1,5 @@
+/home/saazi/Downloads/contributions/rust/on-rust/rustbook/guessing_game/target/debug/deps/guessing_game-8100b1a168adc73f: src/main.rs
+
+/home/saazi/Downloads/contributions/rust/on-rust/rustbook/guessing_game/target/debug/deps/guessing_game-8100b1a168adc73f.d: src/main.rs
+
+src/main.rs:
---
+++ rustbook/guessing_game/target/debug/deps/rand-f0fd9d3200cab3aa.d
@@ -0,0 +1,9 @@
+/home/saazi/Downloads/contributions/rust/on-rust/rustbook/guessing_game/target/debug/deps/rand-f0fd9d3200cab3aa.rmeta: /home/saazi/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /home/saazi/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /home/saazi/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
+
+/home/saazi/Downloads/contributions/rust/on-rust/rustbook/guessing_game/target/debug/deps/librand-f0fd9d3200cab3aa.rlib: /home/saazi/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /home/saazi/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /home/saazi/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
+
+/home/saazi/Downloads/contributions/rust/on-rust/rustbook/guessing_game/target/debug/deps/rand-f0fd9d3200cab3aa.d: /home/saazi/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /home/saazi/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /home/saazi/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
+
+/home/saazi/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs:
+/home/saazi/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs:
+/home/saazi/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs:
